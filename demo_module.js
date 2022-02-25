144a673c736fd4a5037030d6f20974d5e25adddb
skzahid <90752791+skzahid@users.noreply.github.com>
2022-02-25 12:09:23 +0530

    Update demo_module.js

--- demo_module.js
+++ demo_module.js
@@ -9,7 +9,7 @@ http.createServer(function(req,res){
 
 
 
-
+//The Built-in HTTP Module
 
 var http = require('http');
 
@@ -18,3 +18,12 @@ http.createServer(function(req,res){
   res.write('Hello World'); //write a response to the client
   res.end(); //end the response
 }).listen(8080);  //the server object listens on port 8080
+
+//Add an HTTP Header
+var http = require('http');
+
+http.createServer(function (req,res){
+  res.writeHead(200,{'Content-Type': 'text/html'});
+  res.write('Hello world !!!');
+  res.end();
+}).listen(8080);
